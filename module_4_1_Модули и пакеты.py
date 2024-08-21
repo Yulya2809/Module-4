4b5dd9236a2284603167af8e4940f153e42b5c54
Yulya2809 <93946203+Yulya2809@users.noreply.github.com>
2024-08-21 21:53:22 +0300

    Add files via upload

---
+++ module_4_1_Модули и пакеты.py
@@ -0,0 +1,12 @@
+from fake_math import divide as FD
+from true_math import divide as TD
+
+
+result1 = FD(69, 3)
+result2 = FD(3, 0)
+result3 = TD(49, 7)
+result4 = TD(15, 0)
+print(result1)
+print(result2)
+print(result3)
+print(result4)
