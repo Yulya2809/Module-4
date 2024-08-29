d7042e03d4c946f1c74a42a34c03d2ccb1cddb21
Yulya2809 <93946203+Yulya2809@users.noreply.github.com>
2024-08-29 21:38:15 +0300

    Add files via upload

---
+++ module_4_2_Пространство имен.py
@@ -0,0 +1,7 @@
+def test_function():
+    def inner_function():
+        print(f'Я в области видимости функции test_function')
+    inner_function()
+
+
+inner_function()  # функция не работает, ошибка
